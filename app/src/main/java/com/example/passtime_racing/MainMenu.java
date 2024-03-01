97db73bdf067ab0473e4422830738234444f5129
MartPet2 <marpet15@ktu.lt>
2024-03-01 16:55:15 +0200

    First button

---
+++ app/src/main/java/com/example/passtime_racing/MainMenu.java
@@ -0,0 +1,30 @@
+package com.example.passtime_racing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    Button playbutton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        playbutton = (Button) findViewById(R.id.play);
+        playbutton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getBaseContext(), TempGame.class);
+                intent.putExtra("Data", "Laikinas žaidimo meniu");
+                startActivity(intent);
+            }
+        });
+    }
+}
