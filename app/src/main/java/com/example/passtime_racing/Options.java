71f1afe7b1cb313257d7eeab0b762dde745e7686
MartPet2 <marpet15@ktu.lt>
2024-03-01 16:55:15 +0200

    First button

---
+++ app/src/main/java/com/example/passtime_racing/Options.java
@@ -0,0 +1,19 @@
+package com.example.passtime_racing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_game);
+
+        TextView textView = findViewById(R.id.Option_text);
+        String data = getIntent().getStringExtra("Data");
+        textView.setText(data);
+    }
+}
