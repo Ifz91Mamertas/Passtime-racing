a84a727b59b94037678d5a8dff987d06e616ab33
MartPet2 <marpet15@ktu.lt>
2024-03-02 15:17:56 +0200

    Added volume mute button

--- app/src/main/java/com/example/passtime_racing/Options.java
+++ app/src/main/java/com/example/passtime_racing/Options.java
@@ -5,13 +5,16 @@ import android.media.AudioManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.ToggleButton;
 
 public class Options extends AppCompatActivity {
     Button backbutton;
     AudioManager audioManager;
+    ToggleButton togglebutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +29,11 @@ public class Options extends AppCompatActivity {
             public void onClick(View view)
             {
                 Intent intent = new Intent(getBaseContext(), MainMenu.class);
-                //intent.putExtra("Data", "Laikinas žaidimo meniu");
                 startActivity(intent);
             }
         });
+        ToggleButton toggleButton = findViewById(R.id.toggleVolume);
+
         TextView textView = findViewById(R.id.Option_text);
         String data = getIntent().getStringExtra("Data");
         textView.setText(data);
@@ -45,11 +49,6 @@ public class Options extends AppCompatActivity {
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
             }
 
-           // @Override
-            //public void onCheckedChanged(RadioGroup group, int checkedId)
-            //{
-            //}
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar)
             {
@@ -63,8 +62,24 @@ public class Options extends AppCompatActivity {
             }
         }
         );
-       // TextView textView1 = findViewById(R.id.textView2_text);
-        //String data = getIntent().getStringExtra("Data");
-        //textView1.setText(data);
+
+        int volume_level = audioManager.getStreamVolume(audioManager.STREAM_MUSIC);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!toggleButton.isChecked())
+                {
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,0, 0);
+                    seekBar.setProgress(0);
+                    seekBar.setEnabled(false);
+                }
+                else
+                {
+                    seekBar.setProgress(volume_level);
+                    seekBar.setEnabled(true);
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,volume_level, 0);
+                }
+            }
+        });
     }
 }
